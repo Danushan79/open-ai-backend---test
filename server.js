dd182a865cfde8e72ab31336fbfdae9458dede06
danushan <danushan@swivelgroup.com.au>
2024-06-25 15:40:58 +0530

    Updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-
+// comment
 const app = express();
 app.use(express.json());
 app.use("/api/chat/", require("./routes/openApiRoutes"));
